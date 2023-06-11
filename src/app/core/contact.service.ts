f13ab5fd981dfd3d84c49541bfe59e071c0c543d
omniaaymann <omnniao@gmail.com>
2023-06-11 17:26:15 +0300

    MPTS Home

--- src/app/core/contact.service.ts
+++ src/app/core/contact.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ContactService {
-  private api = "https://mailthis.to/omnia";
+  private api = "https://mailthis.to/info@mpts-me.com";
   constructor(private http: HttpClient) { }
 
   SendEmail(input: any) {
